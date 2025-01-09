bea7998fa1db09f252c5627f352053b5a9e45c55
sunat2003 <sunatkumar516@gmail.com>
2025-01-09 09:07:18 +0530

    initial commit

---
+++ calculater2.js
@@ -0,0 +1,28 @@
+btn=document.querySelectorAll(".btn");  //access all the buttons using DOM manupulation
+screenInt=document.querySelector(".screen-int")//access calculator screen
+
+
+//this function will provide the functionality to the each buttons
+btn.forEach(function(ele){
+    ele.addEventListener("click",
+        ()=>{
+            if(ele.value== '=')
+            {
+                screenInt.value=eval(screenInt.value)
+            }
+            else if(ele.value== 'AC'){
+                screenInt.value=""
+            }
+            else if(ele.value== 'DC')
+            {
+                screenInt.value= screenInt.value.slice(0,-1);
+            }
+            else
+            {
+            screenInt.value+=ele.value
+            }
+        }
+    )
+})
+
+console.log(btn);
